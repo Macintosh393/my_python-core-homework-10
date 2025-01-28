c7b14a6c60a33947c1d7ea01651c738e3b4edea9
Max Kosobutskyi <maxkosobutskyi@gmail.com>
2025-01-29 00:08:54 +0200

    working code 1.0.0

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ class Field:
     def __init__(self, value):
         self.value = value
 
-
     def __str__(self):
         return str(self.value)
-    
+
 
 class Name(Field):
     pass
@@ -19,58 +18,53 @@ class Phone(Field):
         if self.validation(value):
             super().__init__(value)
 
-
     def validation(self, phone_num):
         if len(phone_num) == 10 and phone_num.isdigit():
             return True
         else:
-            return False
+            raise ValueError
+
 
 class Record:
-    def __init__(self, name, *args):
+    def __init__(self, name):
         self.name = Name(name)
-        self.phones = [*args]
-
+        self.phones = []
 
     def add_phone(self, phone_num):
         self.phones.append(Phone(phone_num))
-    
 
     def remove_phone(self, phone_num):
         for phone in self.phones:
             if phone.value == phone_num:
                 self.phones.remove(phone)
-    
 
     def edit_phone(self, old_phone_num, new_phone_num):
-        for phone in self.phones:
-            if phone.value == old_phone_num:
-                phone.value = new_phone_num
-    
+        if old_phone_num in [phone.value for phone in self.phones]:
+            for phone in self.phones:
+                if phone.value == old_phone_num:
+                    phone.value = new_phone_num
+        else:
+            raise ValueError
 
     def find_phone(self, phone_num):
         for phone in self.phones:
             if phone.value == phone_num:
                 return phone
 
-
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
-    
+
 
 class AddressBook(UserDict):
     def add_record(self, record):
-        self.data[record.name] = record
-
+        self.data[record.name.value] = record
 
     def find(self, name):
         return self.data.get(name)
 
-
-    def delete(self, user_name):
-        for name, record in self.data.items():
-            if name == user_name:
-                self.data.pop(name)
+    def delete(self, name):
+        if name in self.data.keys():
+            self.data.pop(name)
 
 
 if __name__ == '__main__':
